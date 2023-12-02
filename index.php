f375ccfe2bd508c0bce33eb79ebd554873044d8c
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2023-12-02 03:01:26 -0500

    Add  image

--- index.php
+++ index.php
@@ -1,55 +1,66 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root','');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$statement=$pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="app.css">    
-<title>Products CRUD</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="app.css" rel="stylesheet"/>
+    <title>Products CRUD</title>
 </head>
 <body>
-    <h1>Products CRUD</h1>
-    <table class="table">
-  <thead>
+<h1>Products CRUD</h1>
+
+<p>
+    <a href="create.php" type="button" class="btn btn-sm btn-success">Add Product</a>
+</p>
+<table class="table">
+    <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+        <th scope="col">#</th>
+        <th scope="col">Image</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+        <th scope="col">Create Date</th>
+        <th scope="col">Actions</th>
     </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+    </thead>
+    <tbody>
+    <?php foreach ($products as $i => $product) { ?>
+        <tr>
+            <th scope="row"><?php echo $i + 1 ?></th>
+            <td>
+                <?php if ($product['image']): ?>
+                    <img src="<?php echo $product['image'] ?>" alt="<?php echo $product['title'] ?>" class="product-img">
+                <?php endif; ?>
+            </td>
+            <td><?php echo $product['title'] ?></td>
+            <td><?php echo $product['price'] ?></td>
+            <td><?php echo $product['create_date'] ?></td>
+            <td>
+                <a href="update.php?product_id=<?php echo $product['product_id'] ?>" class="btn btn-sm btn-outline-primary">Edit</a>
+                <form method="post" action="delete.php" style="display: inline-block">
+                    <input  type="hidden" name="product_id" value="<?php echo $product['product_id'] ?>"/>
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+    <?php } ?>
+    </tbody>
 </table>
+
 </body>
 </html>
